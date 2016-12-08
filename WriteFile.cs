7d1187a9354105bea9d346d7f32cb6e3edcda81c
jgilme01 <jgilme01@seattlecentral.edu>
2016-12-08 11:37:06 -0800

    Steve Conger ITC110 Assn 9

---
+++ WriteFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ITC110112916
+{
+    class WriteFile
+    {
+        private StreamWriter writer;
+        string filepath = null;
+
+
+
+
+        public WriteFile(string path)//constructor, always same name as class, never has return type
+        {
+            try
+            {
+                filepath = path;
+                writer = new StreamWriter(path, true);
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void AddText(string line)
+        {
+            writer.Write(line);
+        }
+
+
+        public void CloseFile()
+        {
+            writer.Close();
+        }
+    }
+}
+
+
